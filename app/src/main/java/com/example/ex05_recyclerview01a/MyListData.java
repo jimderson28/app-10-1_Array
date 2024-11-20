a4ce9eb27372fcfe265b0793dbc4af2e5e5b0b56
jimderson28 <jim.chu28@gmail.com>
2024-11-20 13:12:19 +0800

    10-1-Array初始

---
+++ app/src/main/java/com/example/ex05_recyclerview01a/MyListData.java
@@ -0,0 +1,41 @@
+package com.example.ex05_recyclerview01a;
+
+public class MyListData{
+    private String description;
+    private int imgId;
+    public MyListData(String description, int imgId) {
+        this.description = description;
+        this.imgId = imgId;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public int getImgId() {
+        return imgId;
+    }
+    public void setImgId(int imgId) {
+        this.imgId = imgId;
+    }
+
+    public static MyListData[] getMyListDataArray(){
+        MyListData[] myListData = new MyListData[] {
+                new MyListData("Email", android.R.drawable.ic_dialog_email),
+                new MyListData("Info", android.R.drawable.ic_dialog_info),
+                new MyListData("Delete", android.R.drawable.ic_delete),
+                new MyListData("Dialer", android.R.drawable.ic_dialog_dialer),
+                new MyListData("Alert", android.R.drawable.ic_dialog_alert),
+                new MyListData("Map", android.R.drawable.ic_dialog_map),
+                new MyListData("Email", android.R.drawable.ic_dialog_email),
+                new MyListData("Info", android.R.drawable.ic_dialog_info),
+                new MyListData("Delete", android.R.drawable.ic_delete),
+                new MyListData("Dialer", android.R.drawable.ic_dialog_dialer),
+                new MyListData("Alert", android.R.drawable.ic_dialog_alert),
+                new MyListData("Map", android.R.drawable.ic_dialog_map),
+        };
+        return myListData;
+    }
+
+}
